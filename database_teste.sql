f543ab24a2f49ed0d6422fbd4eb7dbaeee3e528e
0x00bit <136848260+0x00bit@users.noreply.github.com>
2024-07-02 10:29:31 -0300

    Update database_teste.sql

--- database_teste.sql
+++ database_teste.sql
@@ -11,4 +11,6 @@ CREATE TABLE "cadastro_clientes" (
 
 \c database_teste;
 
-INSERT INTO "cadastro_clientes" ("nome", "cpf", "date", "telefone") VALUES ("Maria Fulana da Silva", "999.999.999.99", 1999-01-01, "(84)99999-9999");
+INSERT INTO "cadastro_clientes" ("nome", "cpf", "date", "telefone") VALUES ("Maria Fulana da Silva", "999.888.888.88", 1999-01-01, "(84)99999-9999");
+INSERT INTO "cadastro_clientes" ("nome", "cpf", "date", "telefone") VALUES ("Jonas Sicrano Oliveira", "999.888.777.77", 1995-03-11, "(84)92424-2424");
+INSERT INTO "cadastro_clientes" ("nome", "cpf", "date", "telefone") VALUES ("Renata Fulana da Silva", "999.888.777.66", 1999-10-15, "(84)91111-1111");
